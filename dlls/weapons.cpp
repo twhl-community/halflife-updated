b4669215d0bff76b6051a8f79c50917d4c75d277
Solokiller <duo929@hotmail.com>
2021-01-14 18:49:01 +0100

    Fix server not staying in sync with client when handling weapon attack times halflife issue #1621

--- dlls/weapons.cpp
+++ dlls/weapons.cpp
@@ -20,6 +20,8 @@
 
 */
 
+#include <cmath>
+
 #include "extdll.h"
 #include "util.h"
 #include "cbase.h"
@@ -636,7 +638,7 @@ BOOL CanAttack( float attack_time, float curtime, BOOL isPredicted )
 	}
 	else
 	{
-		return ( attack_time <= 0.0 ) ? TRUE : FALSE;
+		return ( attack_time <= 0.0001 ) ? TRUE : FALSE;
 	}
 }
 
@@ -1209,8 +1211,8 @@ float CBasePlayerWeapon::GetNextAttackDelay( float delay )
 	
 	// save the last fire time
 	m_flLastFireTime = gpGlobals->time;		
-	
-	float flNextAttack = UTIL_WeaponTimeBase() + delay - flCreep;
+
+	float flNextAttack = (std::floor((UTIL_WeaponTimeBase() + delay - flCreep) * 1000) / 1000);
 	// we need to remember what the m_flNextPrimaryAttack time is set to for each shot, 
 	// store it as m_flPrevPrimaryAttack.
 	m_flPrevPrimaryAttack = flNextAttack - UTIL_WeaponTimeBase();
