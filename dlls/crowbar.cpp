511c6ef6271692ec391bee61e53addc6dbe79fa6
Solokiller <duo929@hotmail.com>
2021-01-14 13:15:54 +0100

    Fix chainsaw crowbar effect when hitting corpse halflife issue #2590

--- dlls/crowbar.cpp
+++ dlls/crowbar.cpp
@@ -244,6 +244,11 @@ int CCrowbar::Swing( int fFirst )
 		}	
 		ApplyMultiDamage( m_pPlayer->pev, m_pPlayer->pev );
 
+#endif
+
+		m_flNextPrimaryAttack = GetNextAttackDelay(0.25);
+
+#ifndef CLIENT_DLL
 		// play thwack, smack, or dong sound
 		float flVol = 1.0;
 		int fHitWorld = TRUE;
@@ -304,8 +309,6 @@ int CCrowbar::Swing( int fFirst )
 
 		m_pPlayer->m_iWeaponVolume = flVol * CROWBAR_WALLHIT_VOLUME;
 #endif
-		m_flNextPrimaryAttack = GetNextAttackDelay(0.25);
-		
 		SetThink( &CCrowbar::Smack );
 		pev->nextthink = UTIL_WeaponTimeBase() + 0.2;
 
