de2751ca4127d0e98bca1d15e9566d1118fbb1c8
Solokiller <duo929@hotmail.com>
2021-01-31 16:24:47 +0100

    Stop controlling func_tank on disconnect halflife issue #2594

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -132,6 +132,17 @@ void ClientDisconnect( edict_t *pEntity )
 	pEntity->v.solid = SOLID_NOT;// nonsolid
 	UTIL_SetOrigin ( &pEntity->v, pEntity->v.origin );
 
+	auto pPlayer = reinterpret_cast<CBasePlayer*>(GET_PRIVATE(pEntity));
+
+	if (pPlayer)
+	{
+		if (pPlayer->m_pTank != NULL)
+		{
+			pPlayer->m_pTank->Use(pPlayer, pPlayer, USE_OFF, 0);
+			pPlayer->m_pTank = NULL;
+		}
+	}
+
 	g_pGameRules->ClientDisconnected( pEntity );
 }
 
