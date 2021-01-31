17efd5a5eaceac904698d616ea105765c388dec2
Solokiller <duo929@hotmail.com>
2021-01-31 17:38:48 +0100

    Fix weapon prediction allowing multiple reload actions instead of just one halflife bug #2301

--- cl_dll/hl/hl_weapons.cpp
+++ cl_dll/hl/hl_weapons.cpp
@@ -326,9 +326,15 @@ void CBasePlayerWeapon::ItemPostFrame( void )
 {
 	if ((m_fInReload) && (m_pPlayer->m_flNextAttack <= 0.0))
 	{
-#if 0 // FIXME, need ammo on client to make this work right
+#if 1
 		// complete the reload. 
-		int j = V_min( iMaxClip() - m_iClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType]);	
+		ItemInfo ii;
+
+		memset(&ii, 0, sizeof(ii));
+
+		GetItemInfo(&ii);
+
+		int j = V_min(ii.iMaxClip - m_iClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType]);
 
 		// Add them to the clip
 		m_iClip += j;
