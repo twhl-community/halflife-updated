6dd27210d59acf5cce45f4a4caf9b670f6c3d78e
Solokiller <duo929@hotmail.com>
2021-02-06 20:47:36 +0100

    Fix MP5 using wrong spread values for multiplayer and singleplayer halflife issue #2095

--- dlls/mp5.cpp
+++ dlls/mp5.cpp
@@ -153,9 +153,9 @@ void CMP5::PrimaryAttack()
 	Vector vecDir;
 
 #ifdef CLIENT_DLL
-	if ( !bIsMultiplayer() )
+	if ( bIsMultiplayer() )
 #else
-	if ( !g_pGameRules->IsMultiplayer() )
+	if ( g_pGameRules->IsMultiplayer() )
 #endif
 	{
 		// optimized multiplayer. Widened to make it easier to hit a moving player
