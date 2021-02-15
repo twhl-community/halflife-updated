935d9917c8eff62ecd2c5676b4050e42f710babd
Solokiller <duo929@hotmail.com>
2021-02-15 16:27:02 +0100

    Fix scientists not noticing carcasses, meat and garbage halflife issue #2982

--- dlls/scientist.cpp
+++ dlls/scientist.cpp
@@ -778,6 +778,9 @@ int CScientist :: ISoundMask ( void )
 {
 	return	bits_SOUND_WORLD	|
 			bits_SOUND_COMBAT	|
+			bits_SOUND_CARCASS	|
+			bits_SOUND_MEAT		|
+			bits_SOUND_GARBAGE	|
 			bits_SOUND_DANGER	|
 			bits_SOUND_PLAYER;
 }
