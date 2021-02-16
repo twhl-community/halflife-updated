68fe6a74664e257ae5ea217701d78c39599f00a1
Solokiller <duo929@hotmail.com>
2021-02-16 18:59:08 +0100

    Always let server set fov to ensure it restores correctly halflife issue #3044

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -635,8 +635,11 @@ int CHud::MsgFunc_SetFOV(const char *pszName,  int iSize, void *pbuf)
 	int def_fov = CVAR_GET_FLOAT( "default_fov" );
 
 	//Weapon prediction already takes care of changing the fog. ( g_lastFOV ).
+	//But it doesn't restore correctly so this still needs to be used
+	/*
 	if ( cl_lw && cl_lw->value )
 		return 1;
+		*/
 
 	g_lastFOV = newfov;
 
