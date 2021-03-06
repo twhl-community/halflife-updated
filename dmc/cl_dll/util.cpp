3e4e3ff464e349e7630a71e5200e17f5ea675053
Solokiller <duo929@hotmail.com>
2021-03-06 21:38:53 +0100

    Revert "Remove DMC & Ricochet projects"
    This reverts commit f9741671d70080c6026493da0a1df38a621410b6.

---
+++ dmc/cl_dll/util.cpp
@@ -0,0 +1,49 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+//
+// util.cpp
+//
+// implementation of class-less helper functions
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#include "hud.h"
+#include "cl_util.h"
+#include <string.h>
+
+#ifndef M_PI
+#define M_PI		3.14159265358979323846	// matches value in gcc v2 math.h
+#endif
+
+vec3_t vec3_origin( 0, 0, 0 );
+
+HSPRITE LoadSprite(const char *pszName)
+{
+	int i;
+	char sz[256]; 
+
+	if (ScreenWidth < 640)
+		i = 320;
+	else
+		i = 640;
+
+	sprintf(sz, pszName, i);
+
+	return SPR_Load(sz);
+}
+
