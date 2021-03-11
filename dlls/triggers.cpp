66b1f30d524089f5c45102bc8f35071450975e49
Solokiller <duo929@hotmail.com>
2021-03-11 15:06:13 +0100

    Fix trigger_camera movement speed being framerate-dependent halflife issue #2924

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -2156,6 +2156,16 @@ public:
 	int	ObjectCaps() override { return CBaseEntity :: ObjectCaps() & ~FCAP_ACROSS_TRANSITION; }
 	static	TYPEDESCRIPTION m_SaveData[];
 
+	float GetMoveDeltaTime()
+	{
+		if (m_flLastMoveTime == 0)
+		{
+			m_flLastMoveTime = gpGlobals->time - gpGlobals->frametime;
+		}
+
+		return gpGlobals->time - m_flLastMoveTime;
+	}
+
 	EHANDLE m_hPlayer;
 	EHANDLE m_hTarget;
 	CBaseEntity *m_pentPath;
@@ -2169,6 +2179,7 @@ public:
 	float m_acceleration;
 	float m_deceleration;
 	int	  m_state;
+	float m_flLastMoveTime;
 	
 };
 LINK_ENTITY_TO_CLASS( trigger_camera, CTriggerCamera );
@@ -2189,6 +2200,7 @@ TYPEDESCRIPTION	CTriggerCamera::m_SaveData[] =
 	DEFINE_FIELD( CTriggerCamera, m_acceleration, FIELD_FLOAT ),
 	DEFINE_FIELD( CTriggerCamera, m_deceleration, FIELD_FLOAT ),
 	DEFINE_FIELD( CTriggerCamera, m_state, FIELD_INTEGER ),
+	DEFINE_FIELD(CTriggerCamera, m_flLastMoveTime, FIELD_TIME),
 };
 
 IMPLEMENT_SAVERESTORE(CTriggerCamera,CBaseDelay);
@@ -2375,8 +2387,10 @@ void CTriggerCamera::FollowTarget( )
 	if (dy > 180) 
 		dy = dy - 360;
 
-	pev->avelocity.x = dx * 40 * gpGlobals->frametime;
-	pev->avelocity.y = dy * 40 * gpGlobals->frametime;
+	const float deltaTime = GetMoveDeltaTime();
+
+	pev->avelocity.x = dx * 40 * deltaTime;
+	pev->avelocity.y = dy * 40 * deltaTime;
 
 
 	if (!(FBitSet (pev->spawnflags, SF_CAMERA_PLAYER_TAKECONTROL)))	
@@ -2393,12 +2407,16 @@ void CTriggerCamera::FollowTarget( )
 
 void CTriggerCamera::Move()
 {
+	const float deltaTime = GetMoveDeltaTime();
+
+	m_flLastMoveTime = gpGlobals->time;
+
 	// Not moving on a path, return
 	if (!m_pentPath)
 		return;
 
 	// Subtract movement from the previous frame
-	m_moveDistance -= pev->speed * gpGlobals->frametime;
+	m_moveDistance -= pev->speed * deltaTime;
 
 	// Have we moved enough to reach the target?
 	if ( m_moveDistance <= 0 )
@@ -2431,10 +2449,10 @@ void CTriggerCamera::Move()
 	}
 
 	if ( m_flStopTime > gpGlobals->time )
-		pev->speed = UTIL_Approach( 0, pev->speed, m_deceleration * gpGlobals->frametime );
+		pev->speed = UTIL_Approach( 0, pev->speed, m_deceleration * deltaTime);
 	else
-		pev->speed = UTIL_Approach( m_targetSpeed, pev->speed, m_acceleration * gpGlobals->frametime );
+		pev->speed = UTIL_Approach( m_targetSpeed, pev->speed, m_acceleration * deltaTime);
 
-	float fraction = 2 * gpGlobals->frametime;
+	float fraction = 2 * deltaTime;
 	pev->velocity = ((pev->movedir * pev->speed) * fraction) + (pev->velocity * (1-fraction));
 }
