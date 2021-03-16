ffb071658017d51e17242816c4449fbb8d6b487b
Solokiller <duo929@hotmail.com>
2021-03-16 14:04:05 +0100

    Fix func_plat trigger accessing freed memory if platform is removed halflife issue #1725

--- dlls/plats.cpp
+++ dlls/plats.cpp
@@ -251,7 +251,7 @@ public:
 	int	ObjectCaps() override { return (CBaseEntity :: ObjectCaps() & ~FCAP_ACROSS_TRANSITION) | FCAP_DONT_SAVE; }
 	void SpawnInsideTrigger( CFuncPlat *pPlatform );
 	void Touch( CBaseEntity *pOther ) override;
-	CFuncPlat *m_pPlatform;
+	EHANDLE m_hPlatform;
 };
 
 
@@ -350,24 +350,24 @@ static void PlatSpawnInsideTrigger(entvars_t* pevPlatform)
 //
 void CPlatTrigger :: SpawnInsideTrigger( CFuncPlat *pPlatform )
 {
-	m_pPlatform = pPlatform;
+	m_hPlatform = pPlatform;
 	// Create trigger entity, "point" it at the owning platform, give it a touch method
 	pev->solid		= SOLID_TRIGGER;
 	pev->movetype	= MOVETYPE_NONE;
 	pev->origin = pPlatform->pev->origin;
 
 	// Establish the trigger field's size
-	Vector vecTMin = m_pPlatform->pev->mins + Vector ( 25 , 25 , 0 );
-	Vector vecTMax = m_pPlatform->pev->maxs + Vector ( 25 , 25 , 8 );
-	vecTMin.z = vecTMax.z - ( m_pPlatform->m_vecPosition1.z - m_pPlatform->m_vecPosition2.z + 8 );
-	if (m_pPlatform->pev->size.x <= 50)
+	Vector vecTMin = pPlatform->pev->mins + Vector ( 25 , 25 , 0 );
+	Vector vecTMax = pPlatform->pev->maxs + Vector ( 25 , 25 , 8 );
+	vecTMin.z = vecTMax.z - (pPlatform->m_vecPosition1.z - pPlatform->m_vecPosition2.z + 8 );
+	if (pPlatform->pev->size.x <= 50)
 	{
-		vecTMin.x = (m_pPlatform->pev->mins.x + m_pPlatform->pev->maxs.x) / 2;
+		vecTMin.x = (pPlatform->pev->mins.x + pPlatform->pev->maxs.x) / 2;
 		vecTMax.x = vecTMin.x + 1;
 	}
-	if (m_pPlatform->pev->size.y <= 50)
+	if (pPlatform->pev->size.y <= 50)
 	{
-		vecTMin.y = (m_pPlatform->pev->mins.y + m_pPlatform->pev->maxs.y) / 2;
+		vecTMin.y = (pPlatform->pev->mins.y + pPlatform->pev->maxs.y) / 2;
 		vecTMax.y = vecTMin.y + 1;
 	}
 	UTIL_SetSize ( pev, vecTMin, vecTMax );
@@ -379,20 +379,29 @@ void CPlatTrigger :: SpawnInsideTrigger( CFuncPlat *pPlatform )
 //
 void CPlatTrigger :: Touch( CBaseEntity *pOther )
 {
+	//Platform was removed, remove trigger
+	if (!m_hPlatform || !m_hPlatform->pev)
+	{
+		UTIL_Remove(this);
+		return;
+	}
+
 	// Ignore touches by non-players
 	entvars_t*	pevToucher = pOther->pev;
 	if ( !FClassnameIs (pevToucher, "player") )
 		return;
 
 	// Ignore touches by corpses
-	if (!pOther->IsAlive()||!m_pPlatform||!m_pPlatform->pev)
+	if (!pOther->IsAlive())
 		return;
+
+	CFuncPlat* platform = static_cast<CFuncPlat*>(static_cast<CBaseEntity*>(m_hPlatform));
 	
 	// Make linked platform go up/down.
-	if (m_pPlatform->m_toggle_state == TS_AT_BOTTOM)
-		m_pPlatform->GoUp();
-	else if (m_pPlatform->m_toggle_state == TS_AT_TOP)
-		m_pPlatform->pev->nextthink = m_pPlatform->pev->ltime + 1;// delay going down
+	if (platform->m_toggle_state == TS_AT_BOTTOM)
+		platform->GoUp();
+	else if (platform->m_toggle_state == TS_AT_TOP)
+		platform->pev->nextthink = platform->pev->ltime + 1;// delay going down
 }
 
 
