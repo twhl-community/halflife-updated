4c532f484f0aef3e1c5991a40500a48bdcfd7c0f
Sam V <sam.vanheer@outlook.com>
2021-12-16 18:15:09 +0100

    Add particle manager headers to client project, format files #122

--- public/pman_particlemem.h
+++ public/pman_particlemem.h
@@ -8,7 +8,7 @@ class CCoreTriangleEffect;
 
 typedef struct visibleparticles_s
 {
-	CCoreTriangleEffect *pVisibleParticle;
+	CCoreTriangleEffect* pVisibleParticle;
 } visibleparticles_t;
 
 //---------------------------------------------------------------------------
@@ -16,27 +16,27 @@ typedef struct visibleparticles_s
 class MemoryBlock
 {
 private:
-    char *m_pData;
-    //bool m_bBlockIsInUse;
+	char* m_pData;
+	//bool m_bBlockIsInUse;
 public:
-    MemoryBlock(long lBlockSize)
-    : next(NULL), prev(NULL)
-      //m_bBlockIsInUse(false) // Initialize block to 'free' state.
-    {
-        // Allocate memory here.
-        m_pData = new char[lBlockSize];
-    }
+	MemoryBlock(long lBlockSize)
+		: next(NULL), prev(NULL)
+	//m_bBlockIsInUse(false) // Initialize block to 'free' state.
+	{
+		// Allocate memory here.
+		m_pData = new char[lBlockSize];
+	}
 
-    virtual ~MemoryBlock()
-    {
-        // Free memory.
-        delete[] m_pData;
-    }
-  
-    inline char *Memory(void) { return m_pData; }
+	virtual ~MemoryBlock()
+	{
+		// Free memory.
+		delete[] m_pData;
+	}
 
-	MemoryBlock * next;
-	MemoryBlock * prev;
+	inline char* Memory(void) { return m_pData; }
+
+	MemoryBlock* next;
+	MemoryBlock* prev;
 };
 
 class MemList
@@ -46,9 +46,9 @@ public:
 
 	~MemList() { Reset(); }
 
-	void Push(MemoryBlock * newItem)
+	void Push(MemoryBlock* newItem)
 	{
-		if(!m_pHead)
+		if (!m_pHead)
 		{
 			m_pHead = newItem;
 			newItem->next = NULL;
@@ -56,47 +56,47 @@ public:
 			return;
 		}
 
-		MemoryBlock * temp = m_pHead;
+		MemoryBlock* temp = m_pHead;
 		m_pHead = newItem;
 		m_pHead->next = temp;
 		m_pHead->prev = NULL;
-		
+
 		temp->prev = m_pHead;
 	}
-	
-	
-	MemoryBlock * Front( void )
+
+
+	MemoryBlock* Front(void)
 	{
-		return(m_pHead);
+		return (m_pHead);
 	}
 
-	MemoryBlock * Pop( void )
+	MemoryBlock* Pop(void)
 	{
-		if(!m_pHead)
-			return(NULL);
+		if (!m_pHead)
+			return (NULL);
 
-		MemoryBlock * temp = m_pHead;
+		MemoryBlock* temp = m_pHead;
 
 		m_pHead = m_pHead->next;
 
-		if(m_pHead)
+		if (m_pHead)
 			m_pHead->prev = NULL;
 
 		temp->next = NULL;
 		temp->prev = NULL;
 
-		return(temp);
+		return (temp);
 	}
 
-	void Delete( MemoryBlock * pItem)
+	void Delete(MemoryBlock* pItem)
 	{
 
-		if(m_pHead == pItem)
+		if (m_pHead == pItem)
 		{
-			MemoryBlock * temp = m_pHead;
+			MemoryBlock* temp = m_pHead;
 
 			m_pHead = m_pHead->next;
-			if(m_pHead)
+			if (m_pHead)
 				m_pHead->prev = NULL;
 
 			temp->next = NULL;
@@ -104,86 +104,86 @@ public:
 			return;
 		}
 
-		MemoryBlock * prev = pItem->prev;
-		MemoryBlock * next = pItem->next;
+		MemoryBlock* prev = pItem->prev;
+		MemoryBlock* next = pItem->next;
 
-		if(prev)
+		if (prev)
 			prev->next = next;
-		
-		if(next)
+
+		if (next)
 			next->prev = prev;
 
 		pItem->next = NULL;
 		pItem->prev = NULL;
 	}
 
-	void Reset( void )
+	void Reset(void)
 	{
-		while(m_pHead)
+		while (m_pHead)
 			Delete(m_pHead);
 	}
 
 private:
-	MemoryBlock * m_pHead;
+	MemoryBlock* m_pHead;
 };
 
 
 // Some helpful typedefs.
-typedef std::vector<MemoryBlock *> VectorOfMemoryBlocks;
+typedef std::vector<MemoryBlock*> VectorOfMemoryBlocks;
 typedef VectorOfMemoryBlocks::iterator MemoryBlockIterator;
 
 // Mini memory manager - singleton.
 class CMiniMem
 {
 private:
-    // Main memory pool.  Array is fine, but vectors are
-    //  easier. :)
-    static VectorOfMemoryBlocks m_vecMemoryPool;
-    // Size of memory blocks in pool.
-    static long m_lMemoryBlockSize;
-    static long m_lMaxBlocks;
-    static long m_lMemoryPoolSize;
-    static CMiniMem *_instance;
+	// Main memory pool.  Array is fine, but vectors are
+	//  easier. :)
+	static VectorOfMemoryBlocks m_vecMemoryPool;
+	// Size of memory blocks in pool.
+	static long m_lMemoryBlockSize;
+	static long m_lMaxBlocks;
+	static long m_lMemoryPoolSize;
+	static CMiniMem* _instance;
 
 	int m_iTotalParticles;
-    int m_iParticlesDrawn;
+	int m_iParticlesDrawn;
 
 protected:
-    // private constructor and destructor.
-    CMiniMem(long lMemoryPoolSize, long lMaxBlockSize);
-    virtual ~CMiniMem();
+	// private constructor and destructor.
+	CMiniMem(long lMemoryPoolSize, long lMaxBlockSize);
+	virtual ~CMiniMem();
 
-    // ------------ Memory pool manager calls.
-    // Find a free block and mark it as "in use".  Return NULL
-    //  if no free blocks found.
-    char *AllocateFreeBlock(void);
+	// ------------ Memory pool manager calls.
+	// Find a free block and mark it as "in use".  Return NULL
+	//  if no free blocks found.
+	char* AllocateFreeBlock(void);
+
 public:
+	// Return a pointer to usable block of memory.
+	char* newBlock(void);
 
-    // Return a pointer to usable block of memory.
-    char *newBlock(void);
+	// Mark a target memory item as no longer "in use".
+	void deleteBlock(MemoryBlock* p);
 
-    // Mark a target memory item as no longer "in use".
-    void deleteBlock(MemoryBlock *p);
+	// Return the remaining capacity of the memory pool as a percent.
+	long PercentUsed(void);
 
-    // Return the remaining capacity of the memory pool as a percent.
-    long PercentUsed(void);
+	void ProcessAll(void); //Processes all
 
-	void ProcessAll( void ); //Processes all
+	void Reset(void); //clears memory, setting all particles to not used.
 
-	void Reset( void ); //clears memory, setting all particles to not used.
+	static int ApplyForce(Vector vOrigin, Vector vDirection, float flRadius, float flStrength);
 
-	static int ApplyForce( Vector vOrigin, Vector vDirection, float flRadius, float flStrength );
+	static CMiniMem* Instance(void);
+	static long MaxBlockSize(void);
 
-    static CMiniMem *Instance(void);
-    static long MaxBlockSize(void);
+	bool CheckSize(int iSize);
 
-	bool CheckSize( int iSize );
+	int GetTotalParticles(void) { return m_iTotalParticles; }
+	int GetDrawnParticles(void) { return m_iParticlesDrawn; }
+	void IncreaseParticlesDrawn(void) { m_iParticlesDrawn++; }
 
-	int GetTotalParticles( void ) { return m_iTotalParticles;	}
-	int GetDrawnParticles( void ) { return m_iParticlesDrawn;	}
-	void IncreaseParticlesDrawn( void ){ m_iParticlesDrawn++;	}
-	
-	void Shutdown( void );
-	
-	visibleparticles_t *m_pVisibleParticles;
+	void Shutdown(void);
+
+	visibleparticles_t* m_pVisibleParticles;
 };
