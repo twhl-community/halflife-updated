52b6a8a5c3a9062834d7575d7c79c6a78562354d
Sam V <sam.vanheer@outlook.com>
2022-01-23 16:30:58 +0100

    Fix Gauss sound not stopping when players are not in the PAS Resolves ValveSoftware/halflife#3233

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -372,7 +372,7 @@ void CGauss::Fire(Vector vecOrigSrc, Vector vecDir, float flDamage)
 	// It's delayed by a fraction of second to make sure it is delayed by 1 frame on the client
 	// It's sent reliably anyway, which could lead to other delays
 
-	PLAYBACK_EVENT_FULL(FEV_NOTHOST | FEV_RELIABLE, m_pPlayer->edict(), m_usGaussFire, 0.01, m_pPlayer->pev->origin, m_pPlayer->pev->angles, 0.0, 0.0, 0, 0, 0, 1);
+	PLAYBACK_EVENT_FULL(FEV_NOTHOST | FEV_RELIABLE | FEV_GLOBAL, m_pPlayer->edict(), m_usGaussFire, 0.01, m_pPlayer->pev->origin, m_pPlayer->pev->angles, 0.0, 0.0, 0, 0, 0, 1);
 
 
 	/*ALERT( at_console, "%f %f %f\n%f %f %f\n", 
