dcf6c24264bb543f19d11e1dbeaf466022fcb28a
Sam V <sam.vanheer@outlook.com>
2022-03-16 14:12:57 +0100

    Fix "unannotated fallthrough between switch labels" warnings #138

--- external/SDL2/SDL_stdinc.h
+++ external/SDL2/SDL_stdinc.h
@@ -349,9 +349,14 @@ SDL_FORCE_INLINE void SDL_memset4(void *dst, int val, size_t len)
         return;
     switch (len % 4)
     {
-        case 0: do {    *_p++ = _val;
+        case 0:
+            [[fallthrough]];
+            do {    *_p++ = _val;
+			[[fallthrough]];
         case 3:         *_p++ = _val;
+			[[fallthrough]];
         case 2:         *_p++ = _val;
+			[[fallthrough]];
         case 1:         *_p++ = _val;
         } while ( --_n );
     }
