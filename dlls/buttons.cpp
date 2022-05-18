0d101ca3966babba18c101c2108ad009bfa754b0
Sam V <sam.vanheer@outlook.com>
2022-05-18 12:10:50 +0200

    Fix func_rot_button "X Axis" spawnflag also enabling func_button "Sparks" behavior Resolves ValveSoftware/halflife#3270

--- dlls/buttons.cpp
+++ dlls/buttons.cpp
@@ -885,7 +885,8 @@ void CBaseButton::ButtonBackHome()
 		SetTouch(&CBaseButton::ButtonTouch);
 
 	// reset think for a sparking button
-	if (FBitSet(pev->spawnflags, SF_BUTTON_SPARK_IF_OFF))
+	//func_rot_button's X Axis spawnflag overlaps with this one so don't use it here.
+	if (!FClassnameIs(pev, "func_rot_button") && FBitSet(pev->spawnflags, SF_BUTTON_SPARK_IF_OFF))
 	{
 		SetThink(&CBaseButton::ButtonSpark);
 		pev->nextthink = gpGlobals->time + 0.5; // no hurry.
