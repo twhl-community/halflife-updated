ff37029f149849b495f71737f50b3ba8412b0336
Sam V <sam.vanheer@outlook.com>
2023-10-09 15:13:05 +0200

    Fix human grunts continuing to fire for a few seconds after killing the last enemy in an area

--- dlls/hgrunt.cpp
+++ dlls/hgrunt.cpp
@@ -1271,6 +1271,7 @@ Schedule_t slGruntCombatFail[] =
 Task_t tlGruntVictoryDance[] =
 	{
 		{TASK_STOP_MOVING, (float)0},
+		{TASK_SET_ACTIVITY, (float)ACT_IDLE},
 		{TASK_FACE_ENEMY, (float)0},
 		{TASK_WAIT, (float)1.5},
 		{TASK_GET_PATH_TO_ENEMY_CORPSE, (float)0},
