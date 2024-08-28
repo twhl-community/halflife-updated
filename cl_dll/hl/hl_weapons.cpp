952831ccd3fa43bb8f7afba9a457570cb4f219c2
Joël Troch <joel.troch62@gmail.com>
2024-08-28 09:56:30 +0200

    [HL25] Backport changes to view origin/angles

--- cl_dll/hl/hl_weapons.cpp
+++ cl_dll/hl/hl_weapons.cpp
@@ -659,6 +659,8 @@ void HUD_WeaponsPostThink(local_state_s* from, local_state_s* to, usercmd_t* cmd
 	player.m_afButtonPressed = buttonsChanged & cmd->buttons;
 	// The ones not down are "released"
 	player.m_afButtonReleased = buttonsChanged & (~cmd->buttons);
+	player.pev->v_angle = cmd->viewangles;
+	player.pev->origin = from->client.origin;
 
 	// Set player variables that weapons code might check/alter
 	player.pev->button = cmd->buttons;
