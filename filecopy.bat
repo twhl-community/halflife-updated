11341e6f6f438d7f6f05a5f988f7bf5660fc0632
Nabil Benhammou <benhammou.nabil@live.com>
2025-08-10 20:57:02 +0200

    adapt windows and linux copy scripting files to allow other mod files

--- filecopy.bat
+++ filecopy.bat
@@ -2,7 +2,13 @@
 setlocal EnableDelayedExpansion
 
 rem Set this path to your mod directory. You can use environment variables to avoid hardcoding the path if your team members have different install locations.
-set mod_directory=C:/Program Files (x86)/Steam/steamapps/common/Half-Life/halflife_updated
+if NOT DEFINED HL_MOD_PATH (
+	echo hl mod path not defined >&2
+	exit /b 1
+)
+
+set mod_directory=%HL_MOD_PATH%
+set "mod_directory=%mod_directory:\=/%"
 
 rem Input paths may end with a backslash which will be interpreted as an escape character when passed into robocopy, so this needs to be escaped.
 set source=%~1
@@ -18,10 +24,16 @@ set destination=!destination:\=\\!
 
 set filename=%~3
 
+set "options=/njh /njs /ndl /nc /ns /np"
+
+if "%filename%" == "" (
+    set "options=%options% /s"
+)
+
 echo Installing "!destination!/%filename%"
 
 rem If you are having problems with this command, remove the redirect to nul part (> nul) to get error messages from robocopy.
-@robocopy "%source%" "!destination!" "%filename%" /njh /njs /ndl /nc /ns /np > nul
+@robocopy "%source%" "!destination!" "%filename%" %options% > nul
 
 rem Needed because robocopy returns a non-zero code for success which makes Visual Studio treat this as failure.
 exit /b 0
