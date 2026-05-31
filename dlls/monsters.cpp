1cbc565f8562c37236a5bc5901b08edaad650774
Joël Troch <joel.troch62@gmail.com>
2026-05-31 12:51:48 +0200

    Add new line character for "monster stuck in level design" console message

--- dlls/monsters.cpp
+++ dlls/monsters.cpp
@@ -2081,7 +2081,7 @@ void CBaseMonster::StartMonster()
 		// Try to move the monster to make sure it's not stuck in a brush.
 		if (!WALK_MOVE(ENT(pev), 0, 0, WALKMOVE_NORMAL))
 		{
-			ALERT(at_error, "Monster %s stuck in wall--level design error", STRING(pev->classname));
+			ALERT(at_error, "Monster %s stuck in wall--level design error\n", STRING(pev->classname));
 			pev->effects = EF_BRIGHTFIELD;
 		}
 	}
